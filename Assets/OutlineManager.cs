8608a180e4099fc7740c6edbfe55d61ec0d761c5
oirbeboq <revamqq@gmail.com>
2024-12-08 11:26:47 -0500

    Outline fixed

--- Assets/OutlineManager.cs
+++ Assets/OutlineManager.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class OutlineManager : MonoBehaviour
 {
-    public Outline outline;  // Reference to the Outline component
+    public Outline remote;  // Reference to the Outline component
+    public Outline lamp;  // Reference to the Outline component
+    public Outline trash;  // Reference to the Outline component
+    public Outline money;  // Reference to the Outline component
+    public Outline firePlace;
+    public Outline sharpFurniture;
     private bool isFadedOut = true;  // Track if the outline is currently faded out (initially true)
 
     void Start()
     {
         // Start with a basic solid white outline, no fade
-        outline.AnimateOutline(5f, new Color(1f, 1f, 1f, 0f), 0.1f);  // Start with transparent white and fade in
+        remote.AnimateOutline(5f, new Color(1f, 1f, 1f, 0f), 0.1f);  // Start with transparent white and fade in
+        lamp.AnimateOutline(5f, new Color(1f, 1f, 1f, 0f), 0.1f);  // Start with transparent white and fade in
     }
 
     void Update()
@@ -19,12 +25,17 @@ public class OutlineManager : MonoBehaviour
             if (isFadedOut)
             {
                 // Fade in (alpha = 1, visible white)
-                outline.AnimateOutline(5f, new Color(1f, 1f, 1f, 1f), 0.1f);
+                remote.AnimateOutline(5f, new Color(1f, 1f, 1f, 1f), 0.1f);
+                // Fade in (alpha = 1, visible white)
+                lamp.AnimateOutline(5f, new Color(1f, 1f, 1f, 1f), 0.1f);
             }
             else
             {
                 // Fade out (alpha = 0, transparent white)
-                outline.AnimateOutline(5f, new Color(1f, 1f, 1f, 0f), 0.1f);
+                remote.AnimateOutline(5f, new Color(1f, 1f, 1f, 0f), 0.1f);
+
+                // Fade out (alpha = 0, transparent white)
+                lamp.AnimateOutline(5f, new Color(1f, 1f, 1f, 0f), 0.1f);
             }
 
             ScreenShake.instance.TriggerShake(0.2f, 0.4f);
